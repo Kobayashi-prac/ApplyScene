18a6f0dea1818110f5440942a4eb328292d95a33
Kobayashi-prac <df2jm2278f1p6@gmail.com>
2026-03-26 21:04:32 +0900

    First Commit

---
+++ ApplyScene/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ApplyScene
+//
+//  Created by swiftUI on 2026/03/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ApplySceneTests/ApplySceneTests.swift
@@ -0,0 +1,17 @@
+//
+//  ApplySceneTests.swift
+//  ApplySceneTests
+//
+//  Created by swiftUI on 2026/03/26.
+//
+
+import Testing
+@testable import ApplyScene
+
+struct ApplySceneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
