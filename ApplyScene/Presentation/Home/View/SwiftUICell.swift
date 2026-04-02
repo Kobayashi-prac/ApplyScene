34d2d36076cd65195714f6a2a40a03a82564a786
Kobayashi-prac <df2jm2278f1p6@gmail.com>
2026-04-02 22:48:04 +0900

    セルの設定メソッドを定義

--- ApplyScene/Presentation/Home/View/SwiftUICell.swift
+++ ApplyScene/Presentation/Home/View/SwiftUICell.swift
@@ -11,12 +11,29 @@ class SwiftUICell: UITableViewCell {
     
     static let identifier = "SwiftUI"
     
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 16, weight: .semibold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        // styleが何かと毎回呼ばれるか確認
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupComponents()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    func configure(text: String) {
+        titleLabel.text = text
+    }
+    
+    private func setupComponents() {
+        self.contentView.backgroundColor = .lightGray
+        self.contentView.addSubview(titleLabel)
+    }
 }
